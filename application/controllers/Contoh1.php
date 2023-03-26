5c4d60321ae6850c0f2df30030bc479fd1982958
mfachrezzy09 <mfachrezzy04@gmail.com>
2023-03-27 00:23:34 +0700

    Penambahan Contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "Nama saya fachrezzy 
+              Saya tinggal di daerah Cikarang
+              Olahraga yang saya sukai adalah sepak bola";
+    }
+}
