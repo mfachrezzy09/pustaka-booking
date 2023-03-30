dae480f7fc7dfdb00e720b2f40fd5b9357df8949
mfachrezzy09 <mfachrezzy04@gmail.com>
2023-03-30 20:48:06 +0700

    first commit

---
+++ application/models/model_latihan1.php
@@ -0,0 +1,21 @@
+<?php
+class Model_latihan1 extends CI_Model
+{
+    // public $nilai1. $nilai2, $hasil;
+    public function __construct() {
+        //memanggil konstruktor kelas CI_model
+        parent:: __construct();
+        //...
+    }
+        
+    //membuat variable untuk menampung nilai
+    
+    //method penjumlahan
+    public function jumlah($n1 = null, $n2 = null)
+    {
+        $this->nilai1 = $n1;
+        $this->nilai2 = $n2;
+        $this->hasil = $this->nilai1 + $this->nilai2;
+        return $this->hasil;
+    }
+}
